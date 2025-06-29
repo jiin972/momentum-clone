7a007ae9b21c6d0baa8e7fac4cf64137be95e4da
seoungjin lim <30948247+jiin972@users.noreply.github.com>
2025-06-30 04:45:33 +0900

    update for deploy img

--- src/background.js
+++ src/background.js
@@ -1,10 +1,14 @@
-const img = ["img01.jpg", "img02.jpg", "img03.jpg"];
+import img01 from "./assets/img/img01.jpg";
+import img02 from "./assets/img/img02.jpg";
+import img03 from "./assets/img/img03.jpg";
+
+const img = [img01, img02, img03];
 
 const chosenImage = img[Math.floor(Math.random() * img.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `${import.meta.env.BASE_URL}img/${chosenImage}`;
 document.body.appendChild(bgImage);
 
 const overlay = document.createElement("div");
