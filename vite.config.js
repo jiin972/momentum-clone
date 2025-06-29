c27637f29721128b8624ab409ed7663ad667ec14
seoungjin lim <30948247+jiin972@users.noreply.github.com>
2025-06-30 05:41:22 +0900

    final please

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        // 메인 JavaScript 파일은 'main.js'로 이름 고정 (해싱 없이)
         entryFileNames: `assets/[name].js`,
-        // 다른 청크 파일들은 여전히 해싱
         chunkFileNames: `assets/[name]-[hash].js`,
-        // CSS나 이미지 같은 다른 자산 파일들도 여전히 해싱
         assetFileNames: `assets/[name]-[hash].[ext]`,
       },
     },
