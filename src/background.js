ec77b2a11bdda43843c847a69dd5c79f2647f956
seoungjin lim <30948247+jiin972@users.noreply.github.com>
2025-07-01 10:56:03 +0900

    commit for check build code

--- src/background.js
+++ src/background.js
@@ -1,13 +1,7 @@
-import img01 from "./assets/img/img01.jpg";
-import img02 from "./assets/img/img02.jpg";
-import img03 from "./assets/img/img03.jpg";
-
-const img = [img01, img02, img03];
+const img = ["/img/img01.jpg", "/img/img02.jpg", "/img/img03.jpg"];
 
 const chosenImage = img[Math.floor(Math.random() * img.length)];
-
 const bgImage = document.createElement("img");
-
 bgImage.src = chosenImage;
 document.body.appendChild(bgImage);
 
