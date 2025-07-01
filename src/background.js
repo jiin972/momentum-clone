9a9eddf790a9deacc767ecfdfb1a1aa69fd56a3d
seoungjin lim <30948247+jiin972@users.noreply.github.com>
2025-07-01 11:40:22 +0900

    final commit for check build code

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const img = ["/img/img01.jpg", "/img/img02.jpg", "/img/img03.jpg"];
+const img = ["img/img01.jpg", "img/img02.jpg", "img/img03.jpg"];
 
 const chosenImage = img[Math.floor(Math.random() * img.length)];
 const bgImage = document.createElement("img");
